ccd74f1330e4034735798110f57793cfc9c5f06d
B. Daniel <db0.workstation@gmail.com>
2025-08-27 15:59:53 +0200

    Update UnusuedAssetsScanner.cs

--- Assets/Scripts/Utilities/UnusuedAssetsScanner.cs
+++ Assets/Scripts/Utilities/UnusuedAssetsScanner.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
 using System.IO;
@@ -40,3 +41,4 @@ public class UnusedAssetScanner
         Debug.Log($"Report : {outputFile}");
     }
 }
+#endif
